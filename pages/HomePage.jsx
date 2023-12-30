c8c6c37343b32544f0e0713caca82a99ec57c53c
shawn8913 <shawn.shawnliu.liu05@gmail.com>
2023-12-29 23:40:37 -0800

    changed subheading in home page

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
         <div className="page">
             <h1 className="page-header">Component Library</h1>
-            <h2 className="page-subheading">Select the component from the menu above.</h2>
+            <h2 className="page-subheading">Welcome to the React Component Library demo page. Select a component from the top-left dropdown menu.</h2>
         </div>
     )
 }
