8ea841aa1a19aeab30749e716d1545cc60151b0d
shawn8913 <shawn.shawnliu.liu05@gmail.com>
2023-12-30 17:30:52 -0800

    refactored banner component to match toast

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
 
 export default function Banner({title, variant, children}) {
-    let imageSrc;
+    let imageSrc, backgroundColor, titleColor, textColor;
     switch(variant){
         case "success":
-            imageSrc = "../../images/check-circle.svg"
+            imageSrc = "../../images/check-circle.svg";
+            backgroundColor = "#ecf5f5";
+            titleColor = "#065f46";
+            textColor = "#047857";
             break;
         case "warning":
-            imageSrc = "../../images/exclamation.svg"
+            imageSrc = "../../images/exclamation.svg";
+            backgroundColor = "#fffbeb";
+            titleColor = "#92400e";
+            textColor = "#b45309";
             break;
         case "error":
-            imageSrc = "../../images/x-circle.svg"
+            imageSrc = "../../images/x-circle.svg";
+            backgroundColor = "#fef2f2";
+            titleColor = "#92400e";
+            textColor = "#b45309";
             break;
         case "neutral":
-            imageSrc = "../../images/icon.svg"
+            imageSrc = "../../images/icon.svg";
+            backgroundColor = "#eff6ff";
+            titleColor = "#1e40af";
+            textColor = "#1c51b9";
             break;
     }
 
     return (
-        <div className={`banner banner-${variant}`}>
+        <div className="banner" style={{backgroundColor}}>
             <img src={imageSrc} className="banner-icon"/>
             <div className="banner-text-wrapper">
-                <p className="banner-title">{title}</p>
-                {children && <p className="banner-text">{children}</p>}
+                <p className="banner-title" style={{color: titleColor}}>{title}</p>
+                {children && <p className="banner-text" style={{color: textColor}}>{children}</p>}
             </div>
         </div>
     )
